f06c3cfc581e04888f9c4b58682d663dff9de40d
Sam Armstrong <armstrongsam25@gmail.com>
2022-05-24 12:34:12 -0400

    Redid model and code. Now uses CNN+LSTM for ECG5000

--- 1/train_1_ECG5000.py
+++ 1/train_1_ECG5000.py
@@ -1,8 +1,10 @@
 # https://github.com/KristynaPijackova/Tutorials_NNs_and_signals/blob/main/Classification_ECG.ipynb
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
 
 import tensorflow as tf
 from tensorflow import keras
@@ -14,6 +16,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 from imblearn.combine import SMOTETomek
+from sklearn.metrics import classification_report
 import time
 
 
@@ -40,7 +43,7 @@ def over_under_sampling(dataframe):
         # choose samples of i-th class
         df = df_copy[df_copy['c0'] == i]
         # add samples from 1st class
-        df = df.append(df_copy[df_copy['c0'] == 1])
+        df = pd.concat([df, df_copy[df_copy['c0'] == 1]])
         # split the dataframe into x - data and y - labels
         x = df.values[:,1:]
         y = df.values[:,0]
@@ -60,27 +63,33 @@ def over_under_sampling(dataframe):
     # instead of a list of arrays
     return np.concatenate(x_res), np.concatenate(y_res)
 
-def plot_acc(history):
+def plot_acc(history, out_dir):
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'])
-    plt.savefig("./Figures/1_ECG5000_accuracy.png", format="png")
+    fig_path = os.path.join(out_dir, 'figures')
+    if not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+    plt.savefig(os.path.join(fig_path, '1_ECG5000_accuracy.png'), format="png")
     return plt.show()
 
-def plot_loss(history):
+def plot_loss(history, out_dir):
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'])
-    plt.savefig("./Figures/1_ECG5000_loss.png", format="png")
+    fig_path = os.path.join(out_dir, 'figures')
+    if not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+    plt.savefig(os.path.join(fig_path, '1_ECG5000_loss.png'), format="png")
     return plt.show()
 
-def cm_plot(model, x_test):
+def cm_plot(model, x_test, out_dir):
     y_predict = model.predict(x_test)
     y_pred = []
 
@@ -94,8 +103,10 @@ def cm_plot(model, x_test):
     sns.heatmap(cmn, cmap='Blues', annot=True, fmt='.2f')
     sns.set(font_scale=1.3)
     plt.title("Confusion Matrix")
-    plt.savefig("./Figures/1_ECG5000_confusion.png", format="png")
-
+    fig_path = os.path.join(out_dir, 'figures')
+    if not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+    plt.savefig(os.path.join(fig_path, '1_ECG5000_confusion.png'), format="png")
     return plt.show()
 
 def recall_m(y_true, y_pred):
@@ -115,107 +126,162 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-dir', required=True, type=str, default='datasets/ECG5000/', help='Path to the ECG5000 datasets')
+    parser.add_argument('--output-dir', required=True, type=str, default='results/', help='Path to output stuff')
+    parser.add_argument('--num-epochs', type=int, default=100, help='Number of training epochs')
+    parser.add_argument('--batch-size', type=int, default=128, help='Batch size')
+    parser.add_argument('--verbosity', type=int, default=1, help='Verbosity (1-3)')
+    opt = parser.parse_args()
+    return opt
 
-start_time = time.time()
+if __name__ == '__main__':
+    opt = parse_opt()
+    if not os.path.exists(opt.output_dir):
+        os.makedirs(opt.output_dir)
+    if not os.path.exists(os.path.join(opt.output_dir, 'figures')):
+        os.makedirs(os.path.join(opt.output_dir, 'figures'))
+    if not os.path.exists(os.path.join(opt.output_dir, 'logs')):
+        os.makedirs(os.path.join(opt.output_dir, 'logs'))
 
-df_train = pd.read_csv('../datasets/ECG5000/ECG5000_TEST.txt', sep='  ', header=None, engine='python')
-df_test = pd.read_csv('../datasets/ECG5000/ECG5000_TRAIN.txt', sep='  ', header=None, engine='python')
+    start_time = time.time()
 
-df_train = df_train.add_prefix('c')
-df_test = df_test.add_prefix('c')
+    df_train = pd.read_csv(os.path.join(opt.data_dir, 'ECG5000_TEST.txt'), sep='  ', header=None, engine='python')
+    df_test = pd.read_csv(os.path.join(opt.data_dir, 'ECG5000_TRAIN.txt'), sep='  ', header=None, engine='python')
 
-x_os,y_os = over_under_sampling(df_train)
+    df_train = df_train.add_prefix('c')
+    df_test = df_test.add_prefix('c')
 
-x_train = df_train.values[:,1:]  # [all rows, column 1 to end]
-y_train = df_train.values[:,0]   # [all rows, column 0]
+    x_os,y_os = over_under_sampling(df_train)
 
-x_test = df_test.values[:,1:]    # [all rows, column 1 to end]
-y_test = df_test.values[:,0]     # [all rows, column 0]
+    x_train = df_train.values[:,1:]  # [all rows, column 1 to end]
+    y_train = df_train.values[:,0]   # [all rows, column 0]
 
-x_train = np.concatenate((x_train, x_os))
-y_train = np.concatenate((y_train, y_os))
+    x_test = df_test.values[:,1:]    # [all rows, column 1 to end]
+    y_test = df_test.values[:,0]     # [all rows, column 0]
 
-scaler = preprocessing.MinMaxScaler()
-data_scaler = scaler.fit(x_train)
+    x_train = np.concatenate((x_train, x_os))
+    y_train = np.concatenate((y_train, y_os))
 
-x_train = data_scaler.transform(x_train)
-x_test = data_scaler.transform(x_test)
+    scaler = preprocessing.MinMaxScaler()
+    data_scaler = scaler.fit(x_train)
 
-x_train = np.expand_dims(x_train, axis=-1)
-x_test = np.expand_dims(x_test, axis=-1)
+    x_train = data_scaler.transform(x_train)
+    x_test = data_scaler.transform(x_test)
 
-# we need to index classes from 0 to 4
-y_train = y_train - 1            
-y_test = y_test - 1
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
 
-
-x_train, x_val, y_train, y_val = train_test_split(x_train,
-                                                  y_train,
-                                                  test_size=0.2,
-                                                  random_state=42)
-
-layer_in = layers.Input(shape=(140,1))
-layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer_in)
-layer = layers.MaxPool1D(pool_size=2)(layer)
-layer = layers.Dropout(0.2)(layer)
-layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer)
-layer = layers.MaxPool1D(pool_size=2)(layer)
-layer = layers.Dropout(0.2)(layer)
-layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer_in)
-layer = layers.MaxPool1D(pool_size=2)(layer)
-layer = layers.Dropout(0.2)(layer)
-layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer)
-layer = layers.MaxPool1D(pool_size=2)(layer)
-layer = layers.Dropout(0.2)(layer)
-layer = layers.Flatten()(layer)
-layer = layers.Dense(32, activation='relu')(layer)
-layer_out = layers.Dense(5, activation='softmax')(layer)
-
-model = keras.models.Model(layer_in, layer_out)
-
-optimizer = keras.optimizers.Adam(learning_rate=0.001)
-
-callbacks = [
-             keras.callbacks.ModelCheckpoint('ecg_model.h5', 
-                                             save_best_only=True, 
-                                             monitor='val_loss'),
-             keras.callbacks.ReduceLROnPlateau(monitor='val_loss', 
-                                               factor=0.1, 
-                                               patience=3,
-                                               ),
-             keras.callbacks.EarlyStopping(monitor='val_loss', 
-                                           patience=8,
-                                           verbose=1)
-             ]
-
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=optimizer, 
-              metrics=['accuracy'])
+    # we need to index classes from 0 to 4
+    y_train = y_train - 1            
+    y_test = y_test - 1
 
 
-model.summary()
+    x_train, x_val, y_train, y_val = train_test_split(x_train,
+                                                    y_train,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
-history = model.fit(x_train, y_train,
-                    batch_size=64, epochs=100, verbose=2,
-                    validation_data=(x_val, y_val),
-                    shuffle=True, callbacks=callbacks)
+    ######## Only Conv1D ################
+    # layer_in = layers.Input(shape=(140,1))
+    # layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer_in)
+    # layer = layers.MaxPool1D(pool_size=2)(layer)
+    # layer = layers.Conv1D(filters=32, kernel_size=4, activation='relu')(layer)
+    # layer = layers.MaxPool1D(pool_size=2)(layer)
+    # layer = layers.Flatten()(layer)
+    # layer = layers.Dense(32, activation='relu')(layer)
+    # layer = layers.Dropout(0.2)(layer)
+    # layer_out = layers.Dense(5, activation='softmax')(layer)
 
-print("Training time:" + str(time.time() - start_time) + " sec")
+    # model = keras.models.Model(layer_in, layer_out)
 
-start_time = time.time()
+    # optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print("Test accuracy", test_acc)
-print("Test loss", test_loss)
-from sklearn.metrics import classification_report
+    # callbacks = [
+    #              keras.callbacks.ModelCheckpoint('model.h5', 
+    #                                              save_best_only=True, 
+    #                                              monitor='val_loss'),
+    #              keras.callbacks.ReduceLROnPlateau(monitor='val_loss', 
+    #                                                factor=0.1, 
+    #                                                patience=3,
+    #                                                ),
+    #              keras.callbacks.EarlyStopping(monitor='val_loss', 
+    #                                            patience=8,
+    #                                            verbose=1)
+    #              ]
 
-y_pred = model.predict(x_test, batch_size=64, verbose=1)
-y_pred_bool = np.argmax(y_pred, axis=1)
+    # model.compile(loss='sparse_categorical_crossentropy',
+    #               optimizer=optimizer, 
+    #               metrics=['accuracy'])
 
-print("Testing time: " + str(x_test.shape[0] / (time.time() - start_time)) + "samples/sec.")
+    ########### Conv1D and LSTM #############
+    layer_in = layers.Input(shape=(140,1))
+    layer = layers.Conv1D(filters=32, kernel_size=8, activation='leaky_relu')(layer_in)
+    layer = layers.MaxPool1D(pool_size=4)(layer)
+    layer = layers.Bidirectional(layers.LSTM(16, return_sequences=False,))(layer)
+    layer = layers.Dropout(0.2)(layer)
+    layer = layers.Dense(32, activation='leaky_relu')(layer)
+    layer = layers.Dropout(0.2)(layer)
+    layer_out = layers.Dense(5, activation='softmax')(layer)
 
-print(classification_report(y_test, y_pred_bool))
+    model = keras.models.Model(layer_in, layer_out)
 
-plot_acc(history)
-plot_loss(history)
-cm_plot(model, x_test)
+    optimizer = keras.optimizers.Adam(learning_rate=0.001)
+    callbacks = [
+                keras.callbacks.ModelCheckpoint(os.path.join(opt.output_dir, 'ECG500model.h5'), 
+                                                save_best_only=True, 
+                                                monitor='val_loss'),
+                keras.callbacks.ReduceLROnPlateau(monitor='val_loss', 
+                                                factor=0.1, 
+                                                patience=3,
+                                                ),
+                keras.callbacks.EarlyStopping(monitor='val_loss', 
+                                            patience=8,
+                                            verbose=1),
+                keras.callbacks.CSVLogger(os.path.join(opt.output_dir, 'logs', 'log'+str(time.time())+'.csv'), 
+                                            append=True, 
+                                            separator=',')
+                ]
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                optimizer=optimizer, 
+                metrics=['accuracy'])
+
+    model.summary()
+
+    history = model.fit(x_train, y_train,
+                        batch_size=opt.batch_size, epochs=opt.num_epochs, verbose=opt.verbosity,
+                        validation_data=(x_val, y_val),
+                        shuffle=True, callbacks=callbacks)
+
+    train_time = "Training time: " + str(time.time() - start_time) + " sec"
+    print(train_time)
+
+    start_time = time.time()
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    test_acc = "Test accuracy: " + str(test_acc)
+    print(test_acc)
+    test_loss = "Test loss: " + str(test_loss)
+    print(test_loss)
+
+    y_pred = model.predict(x_test, batch_size=opt.batch_size, verbose=opt.verbosity)
+    y_pred_bool = np.argmax(y_pred, axis=1)
+
+    test_time = "Testing time: " + str(x_test.shape[0] / (time.time() - start_time)) + " samples/sec."
+    print(test_time)
+
+    class_report = classification_report(y_test, y_pred_bool)
+    print(class_report)
+    with open(os.path.join(opt.output_dir, 'test_results.txt'), 'w') as f:
+        f.write(train_time + '\n')
+        f.write(test_acc + '\n')
+        f.write(test_loss + '\n')
+        f.write(test_time + '\n')
+        f.write(class_report)
+
+    plot_acc(history, opt.output_dir)
+    plot_loss(history, opt.output_dir)
+    cm_plot(model, x_test, opt.output_dir)
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# ECG Heartbeat Predictor
+
+### Usage
+1. Download the ECG5000 dataset and place it into datasets/ECG5000/<files>
+2. pip install -r requirements.txt
+3. run training/testing script (ex: python ./1/train_1_ECG5000.py)
